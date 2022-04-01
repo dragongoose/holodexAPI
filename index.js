2b8e1c41f1adce5f25e15716fbc3db5b451d7162
dragongoose <dragon@dragongoose.xyz>
2022-03-31 21:26:56 -0400

    Working channel endpoint

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ const fetch = require("node-fetch");
 const Channel = require("./src/Channel.js");
 /**
  * A class representing the holodex api
- * @param {String} key holodex api key
+ * @param {Object} options a object containing the api key
+ * @example { apiKey: 'your api key' }
  */
 class holodex {
-    constructor(key) {
-        this.key = key;
-        this.baseUrl = "https://holodex.app/api/v2";
+    constructor(options) {
+        if(!options || !options.apiKey) {
+            throw new Error('You must provide an api key');
+        }
+        this.key = options.apiKey;
+        this.baseUrl = "https://holodex.net/api/v2";
         this.headers = {
             "Content-Type": "application/json",
-            "X-APIKEY": this.token
+            "X-APIKEY": this.key
         };
         this.fetch = fetch;
     }
@@ -26,12 +30,17 @@ class holodex {
             method: "GET",
             headers: this.headers
         })
-        .then(res => res.json())
-        .catch(err => {
-               throw Error(err) 
-        });
+        .then((data) => {
+            if(data.status === 403) {
+                throw new Error("Invalid API key (Unauthorized)");
+            }
 
-        console.log(data);
+            if(data.status != 200) {
+                throw new Error(data.statusText);
+            }
+
+            return data.json();
+        })
 
         return new Channel(data)
     }
--- test/tests.js
+++ test/tests.js
@@ -1,11 +1,12 @@
 var expect = require('chai').expect;
 const Channel = require('../src/Channel.js');
 const { holodex } = require('../index.js');
-const { showCompletionScript } = require('yargs');
+require('dotenv').config()
+
 
 describe('getChannel()', function() {
     it('should return a channel object', async function() { 
-        const client = new holodex("b7edbc10-018f-4dfc-8048-6ee628cb2cf7")
+        const client = new holodex({ apiKey: process.env.API_KEY });
         let channel = await client.getChannel('UCoSrY_IQQVpmIRZ9Xf-y93g')
 
         console.log(channel)
