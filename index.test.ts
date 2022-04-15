447aae6ddac59ec61500d55bb482c4c0c553adfe
dragongoose <dragon@dragongoose.xyz>
2022-04-15 14:11:53 -0400

    include test

---
+++ index.test.ts
@@ -0,0 +1,10 @@
+import holodex from './index';
+
+const client = new holodex({
+  apiKey: 'b7edbc10-018f-4dfc-8048-6ee628cb2cf7',
+});
+
+test('makes sure getChannel returns channel', async () => {
+  const channel = await client.getChannel('UCoSrY_IQQVpmIRZ9Xf-y93g');
+  expect(channel.englishName).toBe('Gawr Gura');
+});
