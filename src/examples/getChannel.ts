391e075e4c00934bd668482f4d26c76057f7b0f6
dragongoose <dragon@dragongoose.xyz>
2022-04-29 18:43:17 -0400

    Update example

--- src/examples/getChannel.ts
+++ src/examples/getChannel.ts
@@ -1,11 +1,11 @@
 import {holodex} from '../index';
 
 const client = new holodex({
-  apiKey: 'b7edbc10-018f-4dfc-8048-6ee628cb2cf7',
+  apiKey: 'API-KEY-HERE',
 });
 
 client.getChannel('UCoSrY_IQQVpmIRZ9Xf-y93g').then(channel => {
   console.log(channel.name);
   console.log(channel.stats);
-  console.log(channel.type); // 'vtuber'
+  console.log(channel.type);
 });
